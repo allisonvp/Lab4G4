2a19bd661c4ba4d44a1a5c10ae2ec39f55995e9e
katty <a20160555@pucp.edu.pe>
2020-11-24 17:14:19 -0500

    registro ya

--- app/src/main/java/pe/pucp/dduu/tel306/MainActivity.java
+++ app/src/main/java/pe/pucp/dduu/tel306/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,21 +20,27 @@ import android.widget.Toast;
 
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.jar.JarException;
 
 /*import okhttp3.MediaType;
 import okhttp3.Request;
@@ -86,6 +94,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        EditText editTextRegistroNombre = findViewById(R.id.editTextRegistroNombre);
+        EditText editTextRegistroCorreo = findViewById(R.id.editTextRegistroCorreo);
+        EditText editTextRegistroPassword = findViewById(R.id.editTextRegistroPassword);
+
+        String nombre = editTextRegistroNombre.getText().toString();
+        String correo = editTextRegistroCorreo.getText().toString();
+        String password = editTextRegistroPassword.getText().toString();
+
+        /*SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("name", nombre);
+        editor.putString("email", correo);
+        editor.putString("password", password);
+        editor.commit();*/
+
     }
 
     public void fragmentoLogin() {
@@ -109,6 +132,34 @@ public class MainActivity extends AppCompatActivity {
         if (isInternetAvailable()) {
             RequestQueue requestQueue = Volley.newRequestQueue(this);
             String url = "http://34.236.191.118:3000/api/v1/users/login";
+
+            EditText editTextLoginCorreo = findViewById(R.id.editTextLoginCorreo);
+            EditText editTextLoginPassword = findViewById(R.id.editTextLoginPassword);
+
+            String correo = editTextLoginCorreo.getText().toString();
+            String password = editTextLoginPassword.getText().toString();
+
+            Map<String, String> params = new HashMap<String, String>();
+            params.put("email", correo);
+            params.put("password", password);
+
+            JSONObject jsonObj = new JSONObject(params);
+            JsonObjectRequest jsonObjRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObj,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Toast.makeText(MainActivity.this, "se logro", Toast.LENGTH_SHORT).show();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(MainActivity.this, "ggwp", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+            requestQueue.add(jsonObjRequest);
+
             /*StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                     new Response.Listener<String>() {
                         @Override
@@ -143,95 +194,83 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
             requestQueue.add(stringRequest);*/
-
-            EditText editTextLoginCorreo = findViewById(R.id.editTextLoginCorreo);
-            EditText editTextLoginPassword = findViewById(R.id.editTextLoginPassword);
-
-            String correo = editTextLoginCorreo.getText().toString();
-            String password = editTextLoginPassword.getText().toString();
-
-            Map<String, String> params = new HashMap<String, String>();
-            params.put("email", correo);
-            params.put("password", password);
-
-            JSONObject jsonObj = new JSONObject(params);
-            JsonObjectRequest jsonObjRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObj,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            Toast.makeText(MainActivity.this, "se logro", Toast.LENGTH_SHORT).show();
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(MainActivity.this, "ggwp", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-            requestQueue.add(jsonObjRequest);
-
-            /*OkHttpClient client = new OkHttpClient().newBuilder()
-                    .build();
-            MediaType mediaType = MediaType.parse("application/json");
-            RequestBody body = RequestBody.create(mediaType, "{\r\n    \"email\":\"a20160555@pucp.pe\",\r\n    \"password\":\"1234\"\r\n}\r\n");
-            Request request = new Request.Builder()
-                    .url("http://34.236.191.118:3000/api/v1/users/login")
-                    .method("POST", body)
-                    .addHeader("Content-Type", "application/json")
-                    .build();
-            try {
-                Response response = client.newCall(request).execute();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
         }
     }
 
     public void registrarse(View view) {
-        if(isInternetAvailable()) {
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
-            String url = "http://34.236.191.118:3000/api/v1/users/new";
+        if (isInternetAvailable()) {
+            try {
+                RequestQueue requestQueue = Volley.newRequestQueue(this);
+                String url = "http://34.236.191.118:3000/api/v1/users/new";
 
-            EditText editTextRegistroNombre = findViewById(R.id.editTextRegistroNombre);
-            EditText editTextRegistroCorreo = findViewById(R.id.editTextRegistroCorreo);
-            EditText editTextRegistroPassword = findViewById(R.id.editTextRegistroPassword);
+                EditText editTextRegistroNombre = findViewById(R.id.editTextRegistroNombre);
+                EditText editTextRegistroCorreo = findViewById(R.id.editTextRegistroCorreo);
+                EditText editTextRegistroPassword = findViewById(R.id.editTextRegistroPassword);
 
-            String nombre = editTextRegistroNombre.getText().toString();
-            String correo = editTextRegistroCorreo.getText().toString();
-            String password = editTextRegistroPassword.getText().toString();
+                String nombre = editTextRegistroNombre.getText().toString();
+                String correo = editTextRegistroCorreo.getText().toString();
+                String password = editTextRegistroPassword.getText().toString();
 
-            if(nombre.isEmpty()){
-                editTextRegistroNombre.setError("No puede estar vacio");
-            }
-            if(correo.isEmpty()){
-                editTextRegistroCorreo.setError("No puede estar vacio");
-            }
-            if(password.isEmpty()){
-                editTextRegistroPassword.setError("No puede estar vacio");
-            }
+                if (nombre.isEmpty()) {
+                    editTextRegistroNombre.setError("No puede estar vacio");
+                }
+                if (correo.isEmpty()) {
+                    editTextRegistroCorreo.setError("No puede estar vacio");
+                }
+                if (password.isEmpty()) {
+                    editTextRegistroPassword.setError("No puede estar vacio");
+                }
 
-            Map<String, String> params = new HashMap<String, String>();
-            params.put("name", nombre);
-            params.put("email", correo);
-            params.put("password", password);
+                JSONObject jsonBody = new JSONObject();
+                jsonBody.put("name", nombre);
+                jsonBody.put("email", correo);
+                jsonBody.put("password", password);
+                final String mRequestBody = jsonBody.toString();
 
-            JSONObject jsonObj = new JSONObject(params);
-            JsonObjectRequest jsonObjRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObj,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            Toast.makeText(MainActivity.this, "se logro", Toast.LENGTH_SHORT).show();
-                        }
-                    },
-                    new Response.ErrorListener() {
+                if(!nombre.isEmpty() && !correo.isEmpty() && !password.isEmpty()) {
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    Log.d("infoApp", response);
+                                    Toast.makeText(MainActivity.this, "se logro", Toast.LENGTH_SHORT).show();
+                                }
+                            }, new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
+                            Log.d("infoApp", error.toString());
                             Toast.makeText(MainActivity.this, "ggwp", Toast.LENGTH_SHORT).show();
                         }
-                    });
+                    }) {
+                        @Override
+                        public String getBodyContentType() {
+                            return "application/json; charset=utf-8";
+                        }
+                        @Override
+                        public byte[] getBody() throws AuthFailureError {
+                            try {
+                                return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                            } catch (UnsupportedEncodingException uee) {
+                                VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                                return null;
+                            }
+                        }
+                        @Override
+                        protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                            String responseString = "";
+                            if (response != null) {
+                                responseString = String.valueOf(response.statusCode);
+                            }
+                            return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                        }
+                    };
 
-            requestQueue.add(jsonObjRequest);
+                    requestQueue.add(stringRequest);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 
